f0e3ffe90480a7c9ee12ed9784f1c78f13010c0a
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-09 20:11:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
+let myString = 'Algorithm';
 
+// Set 'myString' equal to a substring of itself minus the last letter...
+myString = myString.substring(0, myString.length - 1) +
+// ...and then add the last letter back:
+myString[myString.length - 1];
+
+// myString still contains 'Algorithm'!
+myString;
+// => "Algorithm"
